563992be9faa9bebb71e9d73192c3218f3bc5d1c
Nimira43 <n.rabone72@aol.com>
2026-03-31 22:09:27 +0100

    Crystal butterfly / moth. Archiving  code. Project completed.

--- 02-Test-Areas/fractal-I/script.js
+++ 02-Test-Areas/fractal-I/script.js
@@ -6,23 +6,20 @@ const size = 700
 
 class Fractal {
   constructor(drawMode) {
-    this.lineWidth = Math.floor(Math.random() * 11) + 5
-    this.hue = Math.random() < 0.8 
-      ? Math.random() * 30 + 170
-      : Math.random() * 40 + 280
+    this.lineWidth = Math.floor(Math.random() * 11) + 4
+    this.hue = Math.random() * 360
     this.sides = 6    
-    this.maxLevel = 4
-    this.spread = Math.random() * 0.2 + 0.2
+    this.maxLevel = 5
+    this.spread = Math.random() * 0.3 + 0.3
     this.scale = Math.random() * 0.1 + 0.7
     this.branches = 4
-    this.branchSize = 190
+    this.branchSize = 160
     this.drawMode = drawMode
 
     this.antennaAngle = Math.random() * 0.2 + 0.2
-    this.bodySize = Math.random() * 15 + 10
-    this.upperWingAngle = Math.random() * 0.4 + 1.1
-    this.lowerWingAngle = Math.random() * 0.2 + 2.2
-    this.membraneSize = Math.random() * 1.5 + 2.5
+    this.bodySize = Math.random() * 20 + 10
+    this.upperWingAngle = Math.random() * 0.4 + 1
+    this.lowerWingAngle = Math.random() * 0.3 + 2.1
   }
 
   draw(ctx) {
@@ -30,7 +27,7 @@ class Fractal {
     ctx.save()
     ctx.translate(canvas.width / 2, canvas.height / 2)
 
-    ctx.fillStyle = `hsl(${this.hue - 10}, 100%, 50%)`
+    ctx.fillStyle = `hsl(0, 100%, 54%)`
     const numCircles = this.bodySize
     const bodyLength = this.branchSize
     const spacing = bodyLength / (numCircles - 1)
@@ -38,7 +35,7 @@ class Fractal {
     for (let i = numCircles - 1; i > 0; i--) {
       const y = i * spacing - bodyLength / 2
       const progress = i / (numCircles - 1)
-      const sizeMultiplier = Math.abs(Math.sin(progress * Math.PI * 2.6))
+      const sizeMultiplier = Math.sin(progress * Math.PI) * (Math.random() * 0.5 + 0.7)
       const radius = this.bodySize * (0.3 + sizeMultiplier)
       ctx.beginPath()
       ctx.arc(0, y, radius, 0, Math.PI * 2)
@@ -48,15 +45,15 @@ class Fractal {
     ctx.save()
     ctx.translate(-this.bodySize / 2, -60)
     ctx.rotate(-Math.PI / 2 - this.antennaAngle)
-    ctx.scale(0.8, 0.7)
-    this.drawBranch(ctx, this.maxLevel - 3)
+    ctx.scale(0.8, -0.7)
+    this.drawBranch(ctx, this.maxLevel - 2)
     ctx.restore()
     
     ctx.save()
     ctx.translate(this.bodySize / 2, -60)
     ctx.rotate(-Math.PI / 2 + this.antennaAngle)
-    ctx.scale(0.8, -0.7)
-    this.drawBranch(ctx, this.maxLevel - 3)
+    ctx.scale(0.8, 0.7)
+    this.drawBranch(ctx, this.maxLevel - 2)
     ctx.restore()
     
     ctx.save()
@@ -96,17 +93,20 @@ class Fractal {
     const lightness = 10 + level * 10
     
     if (this.drawMode === 'branches') {
-      ctx.save()
-      ctx.strokeStyle = `hsl(${this.hue}, 100%, ${lightness}%)`
-      ctx.beginPath()
-      ctx.moveTo(0, 0)
-      ctx.lineTo(this.branchSize, 0)
-      ctx.stroke()
-      ctx.restore()
+      if (level < this.maxLevel - 1) {
+        ctx.save()
+        ctx.strokeStyle = `hsl(0, 100%, 32%)`
+        ctx.lineWidth = this.lineWidth * 1.5
+        ctx.beginPath()
+        ctx.moveTo(0, 0)
+        ctx.lineTo(this.branchSize, 0)
+        ctx.stroke()
+        ctx.restore()
+      }
     }
     
     else if (this.drawMode === 'circles') {
-      ctx.fillStyle = `hsl(${this.hue + 10}, 100%, ${lightness + 5}%)`
+      ctx.fillStyle = `hsl(${this.hue + 10 }, 100%, ${lightness + 5}%)`
       ctx.beginPath()  
       ctx.arc(this.branchSize, 0, this.lineWidth * 1.5, 0, Math.PI * 2)
       ctx.fill()
@@ -137,7 +137,7 @@ class Fractal {
         ctx.shadowColor = `hsl(${this.hue}, 100%, 90%)`
         ctx.shadowBlur = 4
         ctx.shadowOffsetX = 0
-        ctx.shadowOffsetY = 0
+        ctx.shadowOffsetY = 6
         ctx.beginPath()
         ctx.moveTo(0, 0)
         ctx.lineTo(this.branchSize * 0.2, Math.random() * 20 - 10)
@@ -150,97 +150,81 @@ class Fractal {
       }
     }
 
-    else if (this.drawMode === 'membrane') {
-      if (level === 0) {
+    else if (this.drawMode === 'glow') {
+      if (level < this.maxLevel) {
         ctx.save()
-        const gradient = ctx
-          .createRadialGradient(0, 0, 0, 0, 0, this.branchSize * 3.5)
-        gradient
-          .addColorStop(0, `hsla(${this.hue}, 80%, 60%, 0.35)`)
-        gradient
-          .addColorStop(0.6, `hsla(${this.hue}, 70%, 50%, 0.15)`)
-        gradient
-          .addColorStop(1, `hsla(${this.hue}, 60%, 40%, 0.02)`)
-      
-        ctx.fillStyle = gradient
+        ctx.strokeStyle = `hsla(0, 100%, 54%, 0.8)`
+        ctx.lineWidth = this.lineWidth * Math.random() * 4 + 10
+        ctx.shadowColor = `hsla(0, 100%, 54%, 0.8)`
+        ctx.shadowBlur = 60
         ctx.beginPath()
         ctx.moveTo(0, 0)
-
-        for (let t = 0; t <= 1; t += 0.02) {
-          const x = this.branchSize * t * this.membraneSize
-          const width = this.branchSize * 1.2 * Math.sin(t * Math.PI)
-          ctx.lineTo(x, width)
-        }
-        
-        for (let t = 1; t >= 0; t -= 0.02) {
-          const x = this.branchSize * t * this.membraneSize
-          const width = this.branchSize * 1.2 * Math.sin(t * Math.PI)
-          ctx.lineTo(x, -width)
-        }
-
-        ctx.closePath()
-        ctx.fill()
-        ctx.lineWidth = 1
-        ctx.strokeStyle = `hsla(${this.hue + 20}, 100%, 70%, 0.25)`
+        ctx.quadraticCurveTo(this.branchSize * 0.5, Math.random() * 80 - 40, this.branchSize * 1.5, 0)
         ctx.stroke()
-        ctx.restore() 
-      }
-    }
-
-    else if (this.drawMode === 'bioluminescence') {
-      if (level > this.maxLevel - 3 && Math.random() < 0.1) {
-        ctx.save()
-        const xPos = Math.random() * this.branchSize
-        const yPos = (Math.random() - 0.5) * 20
-        const dotSize = Math.random() * 8 + 4
-        
-        const gradient = ctx
-          .createRadialGradient(xPos, yPos, 0, xPos, yPos, dotSize * 4)
-        gradient
-          .addColorStop(0, `hsla(${this.hue}, 100%, 90%, 0.9)`)
-        gradient
-          .addColorStop(0.5, `hsla(${this.hue}, 100%, 60%, 0.4)`)
-        gradient
-          .addColorStop(1, `hsla(${this.hue}, 100%, 40%, 0)`)
-        
-        ctx.fillStyle = gradient
-        ctx.shadowColor = `hsla(${this.hue}, 100%, 70%, 0.8)`
-        ctx.shadowBlur = 8
-        ctx.beginPath()
-        ctx.arc(xPos, yPos, dotSize * 4, 0, Math.PI * 2)
-        ctx.fill()
-        
-        ctx.fillStyle = `hsla(${this.hue + 20}, 100%, 95%, 1)`
-        ctx.beginPath()
-        ctx.arc(xPos, yPos, dotSize, 0, Math.PI * 2)
-        ctx.fill()
         ctx.restore()
       }
     }
 
-    else if (this.drawMode === 'scales') {
-      if (level > this.maxLevel - 1 && Math.random() > 0.2) {
-        ctx.save()
-        const size = Math.random() * 15 + 15
-        ctx.lineWidth = 3
-        ctx.fillStyle = `hsla(${this.hue + 10}, 100%, 40%, 0.5)`
-        ctx.strokeStyle = `hsla(${this.hue}, 100%, 60%, 0.8)`
+    else if (this.drawMode === 'veins') {
+      if (level < this.maxLevel) {
+        ctx.strokeStyle = `hsl(0, 100%, 20%)`
+        ctx.lineWidth = 4
         ctx.beginPath()
-        ctx.translate(size, 0)                                  
-        ctx.moveTo(this.branchSize, -size)
-        ctx.lineTo(this.branchSize + size * (Math.random() * 10 + 10), 0)
-        ctx.lineTo(this.branchSize, size)
-        ctx.lineTo(this.branchSize + size, 0)
-        ctx.closePath()
-        ctx.fill()
+        ctx.moveTo(0, 0)
+        const cp1x = this.branchSize * 0.33
+        const cp1y = (Math.random() - 0.5) * 80
+        const cp2x = this.branchSize * 0.66
+        const cp2y = (Math.random() - 0.5) * 80
+        ctx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, this.branchSize, 0)
         ctx.stroke()
+      }
+    }
+
+    else if (this.drawMode === 'tendrils') {
+      ctx.save()
+
+      if (level < this.maxLevel && level > 2 && Math.random() < 0.25) {
+        ctx.strokeStyle = `hsl(0, 100%, 20%)`
+        ctx.lineWidth = this.lineWidth
+        ctx.shadowColor = `hsla(0, 100%, 50%, 0.6)`
+        ctx.shadowBlur = 20
+        ctx.beginPath()
+        ctx.moveTo(0, 0)
+        const length = this.branchSize * 4
+        const segments = 20
+        const waveFreq = 2
+        const waveAmp = 40
+
+        for (let i = 1; i <= segments; i++) {
+          const t = i / segments
+          const x = length * t
+          const y = Math.sin(t * Math.PI * waveFreq) * waveAmp
+          ctx.lineTo(x, y)
+        }
+        ctx.stroke()
+        ctx.strokeStyle = `rgba(255, 255, 255, 0.2)`
+        ctx.lineWidth = this.lineWidth * 0.3
+        ctx.stroke()
+      }
+      ctx.restore()
+    }
+
+    else if (this.drawMode === 'spores') {
+      if (level > 1 && level < this.maxLevel && Math.random() < 0.5) {
+        ctx.save()
+        ctx.fillStyle = `hsl(0, 100%, 15%)`
+        ctx.shadowColor = `hsla(0, 100%, 55%, 0.8)`
+        ctx.shadowBlur = 20
+        ctx.beginPath()
+        ctx.arc(this.branchSize + Math.random() * this.branchSize * 4, 0, Math.random() * 10 + 2, 0, Math.PI * 2) 
+        ctx.fill()
         ctx.restore()
       }
     }
     
     for (let i = 0; i < this.branches; i++) {
       const position = this.branchSize - (this.branchSize / this.branches) * i
-      const angle = this.spread * 2.1 * i * 1.3 / this.branches - this.spread * 0.95
+      const angle = this.spread * 2.1 * i / this.branches - this.spread * 0.95
 
       ctx.save()
       ctx.translate(position, 0)
@@ -248,28 +232,21 @@ class Fractal {
       ctx.rotate(angle)
       this.drawBranch(ctx, level + 1)
       ctx.restore()
-
-      if (level < this.maxLevel - 3) {
-        ctx.save()
-        ctx.translate(position, 0)
-        ctx.scale(this.scale * 0.7, this.scale * 0.75)
-        ctx.rotate(-angle)
-        this.drawBranch(ctx, level + 1)
-        ctx.restore()
-      }
     }
   }
 }
 
 function drawFractal() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
-  const fractal = new Fractal('membrane')
+  const fractal = new Fractal('glow')
   fractal.draw(ctx)
   fractal.drawMode = 'branches'
   fractal.draw(ctx)
-  fractal.drawMode = 'scales'
+  fractal.drawMode = 'veins'
   fractal.draw(ctx)
-  fractal.drawMode = 'bioluminescence'
+  fractal.drawMode = 'tendrils'
+  fractal.draw(ctx)
+  fractal.drawMode = 'spores'
   fractal.draw(ctx)
   
   // fractal.drawMode = 'electricity'
@@ -286,7 +263,7 @@ function resizeCanvas() {
   canvas.width = window.innerWidth
   canvas.height = window.innerHeight
   ctx.lineCap = 'round'
-  ctx.shadowColor = 'rgba(0, 0, 0, 0.5)'
+  ctx.shadowColor = 'hsla(0, 100%, 10%, 0.3)'
   ctx.shadowBlur = 20
   ctx.shadowOffsetX = 3
   ctx.shadowOffsetY = 3
